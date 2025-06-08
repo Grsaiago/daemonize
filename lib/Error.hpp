ae743ad9ce2e205e62e1a8fea1b4ff386da24ba6
Gabriel Saiago <grsaiago@gmail.com>
2025-06-08 20:03:06 -0300

    delete: Result unused definitions

--- lib/Error.hpp
+++ lib/Error.hpp
@@ -2,37 +2,10 @@
 # define _RESULT_H_
 # include <string>
 
-
 class Error {
 public:
 	Error(std::string reason) : reason(reason) {}
 	const std::string	reason;
-};
-
-enum class ResultStates {
-	Ok,
-	Err
-};
-
-template <typename T>
-class Result {
-public:
-	static Result	Ok(T value);
-	static Result	Err(std::string reason);
-
-	~Result();
-
-	bool	is_ok(void) const noexcept;
-	bool	is_err(void) const noexcept;
-	T	get_ok() noexcept(false);
-	Error	get_err() noexcept(false);
-private:
-	Result();
-	ResultStates	current_state;
-	union {
-		T	ok;
-		Error	err;
-	} current_value;
 };
 
 #endif // _RESULT_H_
